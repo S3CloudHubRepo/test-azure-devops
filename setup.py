e930e08d740c09a15853526f6cda7eabc5dbaf83
S3CloudHub <129742617+S3CloudHubRepo@users.noreply.github.com>
2024-10-01 15:38:05 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='your_package_name',
+    version='0.1',
+    packages=['your_package'],
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
